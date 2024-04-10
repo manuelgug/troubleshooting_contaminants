b99f51325c24c474c7478159c7fa172179ef934d
Manuel García-Ulloa <36465310+manuelgug@users.noreply.github.com>
2024-04-10 17:56:37 +0200

    first working prototype COMPLETE

--- QC.R
+++ QC.R
@@ -83,16 +83,16 @@ if (pool == "2"){
   }
 
 # wide df format
-allele_data_df <- pivot_wider(allele_data_df, names_from = locus_allele, values_from = reads)
+allele_data_df_wide <- pivot_wider(allele_data_df, names_from = locus_allele, values_from = reads)
 
-# Replace NA with 0 in allele_data_df
-allele_data_df[is.na(allele_data_df)] <- 0
+# Replace NA with 0 in allele_data_df_wide
+allele_data_df_wide[is.na(allele_data_df_wide)] <- 0
 
 
 
 
 ## PcoA of allele pres/abs of all samples from all runs
-allele_data_df_pres_abs <- allele_data_df
+allele_data_df_pres_abs <- allele_data_df_wide
 allele_data_df_metadata <- allele_data_df_pres_abs[,1:2] #separate metadata from values
 allele_data_df_pres_abs <- allele_data_df_pres_abs[,-1:-2]
 allele_data_df_pres_abs[allele_data_df_pres_abs > 1] <- 1 #convert data to presence/absence
@@ -216,6 +216,7 @@ pca_merged <- grid.arrange(pca_plot_proportions, pca_plot_pres_abs, ncol = 2)
 ggsave(paste0(problem_run, "_samples_PCA.png"), pca_merged, width = 12, height = 6)
 
 
+
 #### 3) Check presence of REF alleles from 3d7 controls
 
 #subset problem run again because fuck efficiency
@@ -243,3 +244,131 @@ write.csv(controls_3D7_ref_counts, paste0(problem_run, "_controls_3D7.csv"), row
 
 
 #### 4) Compare alleles from neg controls against all samples from previous and current run to detect potential sources of contaminants
+#find neg control names in problem_run
+problem_run_df_again_again <- allele_data_df[allele_data_df$run == problem_run, ]
+neg_controls <- unique(problem_run_df_again_again$sampleID[grepl("(?i)Neg",problem_run_df_again_again$sampleID)])
+
+#extract data from neg controls
+problem_run_df_again_again_neg_controls <- problem_run_df_again_again[problem_run_df_again_again$sampleID %in% neg_controls,]
+
+#count unique alleles on each neg control
+n_alleles_in_neg_controls <- problem_run_df_again_again_neg_controls %>% 
+  group_by(sampleID) %>%
+  summarize(length(unique(locus_allele)))
+
+
+# Create an empty list to store sorted_combined_df for each neg_control
+sorted_combined_dfs <- list()
+
+# Loop through each element in neg_controls_asntmal
+for (neg_control in neg_controls) {
+  # Subset problem_run_df_again_again_neg_controls for the current neg_control
+  problem_run_df_again_again_neg_controls_subset <- problem_run_df_again_again_neg_controls[grepl(neg_control, problem_run_df_again_again_neg_controls$sampleID), ]
+  
+  l <- length(unique(problem_run_df_again_again_neg_controls_subset$locus_allele))
+  
+  # Initialize an empty list to store results for each run
+  run_results <- list()
+  
+  # Loop through each unique run
+  for (run in unique(allele_data_df$run)) {
+    # Subset the data for the current run
+    run_data <- subset(allele_data_df, run == run)
+    
+    # Perform the required operations for the current run
+    result <- run_data %>% 
+      group_by(sampleID) %>% 
+      summarize(
+        n_alleles_shared = sum(unique(locus_allele) %in% unique(problem_run_df_again_again_neg_controls_subset$locus_allele)),
+        perc_alleles_shared = sum(unique(locus_allele) %in% unique(problem_run_df_again_again_neg_controls_subset$locus_allele)) / l,
+        run = run
+      ) %>%
+      arrange(desc(perc_alleles_shared))
+    
+    # Store the result for the current run
+    run_results[[run]] <- result
+  }
+  
+  # Combine the results for all runs into a single dataframe
+  combined_df <- do.call(rbind, run_results)
+  
+  # Rename columns and adjust percentages
+  combined_df$perc_alleles_shared <- ifelse(combined_df$perc_alleles_shared > 1, 1, combined_df$perc_alleles_shared)
+  colnames(combined_df)[2:3] <- paste0(colnames(combined_df)[2:3], "_", neg_control)
+  
+  # Add the result to the list with the name of the neg_control
+  sorted_combined_dfs[[neg_control]] <- combined_df
+}
+
+
+
+#nrow(sorted_combined_dfs[[1]]) #holy shit
+sorted_combined_dfs <- lapply(sorted_combined_dfs, distinct) #lazy cope, shouldn't generate that amount of repeated rows!!!
+
+
+
+# Merge all data frames in sorted_combined_dfs by sampleID and run
+merged_df <- Reduce(function(x, y) merge(x, y, by = c("sampleID", "run"), all = TRUE), sorted_combined_dfs)
+
+# Extract columns percentage
+selected_cols <- c("sampleID", "run", grep("perc", colnames(merged_df), value = TRUE))
+
+# Subset the dataframe
+selected_df_perc <- merged_df[, selected_cols]
+
+selected_df_perc$mean_perc_shared_alleles<- rowMeans(selected_df_perc[,3:ncol(selected_df_perc)])
+
+selected_df_perc <- selected_df_perc %>%
+  arrange(desc(mean_perc_shared_alleles))
+
+selected_df_perc[,c(1,2,ncol(selected_df_perc))]
+
+# Remove duplicates from sampleID column
+unique_sampleID <- unique(selected_df_perc$sampleID)
+
+# Reorder levels based on mean_perc_shared_alleles
+sorted_levels <- unique_sampleID[order(-selected_df_perc$mean_perc_shared_alleles[match(unique_sampleID, selected_df_perc$sampleID)])]
+
+# Set sampleID as factor with reordered levels
+selected_df_perc$sampleID <- factor(selected_df_perc$sampleID, levels = sorted_levels)
+
+#remove undetermined since they don't provide any info on source of contamination
+selected_df_perc <- selected_df_perc[selected_df_perc$sampleID !="Undetermined_S0_L001",]
+
+write.csv(selected_df_perc, paste(problem_run, "_percentages_contaminants.csv"), row.names = F)
+
+
+
+#keep 200 more likely samples for easier visualization
+selected_df_perc_subset <- selected_df_perc[1:500,]
+
+set.seed(123)
+
+# Select the unique runs
+unique_runs <- unique(pc_scores$run)
+n_runs <- length(unique_runs)
+
+# Number of colors to generate
+n_colors <- 50
+
+# Generate random colors in hexadecimal format
+random_colors <- rainbow(n_runs)
+
+# Repeat the sampled colors to match the number of runs
+# Generate a palette of colors
+color_palette <- rainbow(n_runs)
+
+# Repeat the palette to match the number of unique runs
+color_palette <- rep(color_palette, length.out = n_runs)
+
+ggplot(selected_df_perc, aes(x = 1:length(sampleID), y = mean_perc_shared_alleles, color = run)) +
+  geom_point(size =3, alpha = 0.7) +
+  scale_color_manual(values = color_palette) +
+  labs(title = "Mean Percentage of Shared Alleles by Sample ID",
+       x = "Sample ID",
+       y = "Mean Percentage of Shared Alleles",
+       color = "Run") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  guides(color = guide_legend(ncol = 1))
+
